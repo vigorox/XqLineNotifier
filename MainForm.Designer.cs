e73d3944cee3edd555527f307fda1d595b9d859a
DESKTOP-D97ETG9\vigor <vigorox@gmail.com>
2022-12-28 10:56:47 +0800

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace XqLineNotifier
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtLineToken = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(357, 36);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "儲存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtLineToken
+            // 
+            this.txtLineToken.Location = new System.Drawing.Point(34, 36);
+            this.txtLineToken.Name = "txtLineToken";
+            this.txtLineToken.Size = new System.Drawing.Size(317, 20);
+            this.txtLineToken.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Line Token";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(340, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "預設請將此程式置於C:\\XqLog\\處執行，並填入正確的Line Token";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 112);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLineToken);
+            this.Controls.Add(this.btnSave);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "XQ Line Notifier";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtLineToken;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace XqLineNotifier
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private string lineToken = "";
+        private static string AppJsonFilePath = "./lineToken.json";
+        private static string XqMonitorPath = "./";
+        private static string XqLogFileExtension = "*.xqlog";
+
+        public void CreateFileWatcher(string path)
+        {
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = path;
+            /* Watch for changes in LastAccess and LastWrite times, and 
+               the renaming of files or directories. */
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            // Add event handlers.
+            // Only watch *.xqlog files
+            watcher.Filter = XqLogFileExtension;
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnCreatedAsync);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+        }
+
+        // Define the event handlers.
+        private async void OnCreatedAsync(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+            
+            try
+            {
+                if (this.lineToken.Trim() != "")
+                {
+                    string message = File.ReadAllText(e.FullPath, Encoding.GetEncoding("Big5"));
+
+                    HttpClient client = new HttpClient();
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.lineToken);
+                    var form = new FormUrlEncodedContent(new[]
+                    {
+                    new KeyValuePair<string, string>("message", message)
+                });
+                    HttpResponseMessage result = await client.PostAsync(new Uri("https://notify-api.line.me/api/notify"), form);
+                }
+                File.Delete(e.FullPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Specify what is done when a file is changed, created, or deleted.
+            Console.WriteLine("File: " + e.FullPath + " " + e.ChangeType);
+        }
+
+        private static void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Specify what is done when a file is renamed.
+            Console.WriteLine("File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+        }
+
+        public class AppSettings
+        {
+            public string LineToken { get; set; }
+        }
+
+        private AppSettings readAppSettings()
+        {
+            var jsonString = System.IO.File.ReadAllText(AppJsonFilePath);
+            AppSettings appSettings = JsonConvert.DeserializeObject<AppSettings>(jsonString);
+            return appSettings;
+        }
+
+        private void checkXqLineNotifyPathExists()
+        {
+            if (!Directory.Exists(XqMonitorPath))
+            {
+                Directory.CreateDirectory(XqMonitorPath);
+            }
+        }
+        private void createAppSettingsFile()
+        {
+            var appJsonObj = new AppSettings() { LineToken = "" };
+            System.IO.File.WriteAllText(AppJsonFilePath, JsonConvert.SerializeObject(appJsonObj));
+        }
+
+        private void initAppSettings()
+        {
+
+            try
+            {
+                if (!System.IO.File.Exists(AppJsonFilePath))
+                {
+                    this.createAppSettingsFile();
+                }
+                AppSettings appSettings = this.readAppSettings();
+                lineToken = appSettings.LineToken;
+                txtLineToken.Text = lineToken;
+            }
+            catch (Exception e)
+            {
+                this.createAppSettingsFile();
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            checkXqLineNotifyPathExists();
+            initAppSettings();
+            createXqMonitorPath();
+            deleteAllXqLog();
+            CreateFileWatcher(XqMonitorPath);
+        }
+
+        private void createXqMonitorPath() {
+            if (!System.IO.Directory.Exists(XqMonitorPath)) {
+                System.IO.Directory.CreateDirectory(XqMonitorPath);
+            }
+        }
+
+        private void deleteAllXqLog() {
+            foreach (string sFile in System.IO.Directory.GetFiles(XqMonitorPath, XqLogFileExtension))
+            {
+                System.IO.File.Delete(sFile);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            txtLineToken.Text = txtLineToken.Text.Trim();
+            AppSettings appSettings = this.readAppSettings();
+            appSettings.LineToken = txtLineToken.Text;
+            string jsonString = JsonConvert.SerializeObject(appSettings);
+            System.IO.File.WriteAllText(AppJsonFilePath, jsonString);
+            this.lineToken = txtLineToken.Text;
+        }
+    }
+}
